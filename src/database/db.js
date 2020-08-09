962a9c8d2f0a2b60220257c2547fb7d606bd927e
GutoKophal <17kophal@gmail.com>
2020-08-09 16:51:28 -0300

    Finalizing the SQL

--- src/database/db.js
+++ src/database/db.js
@@ -1,8 +1,7 @@
 const Database = require('sqlite-async')
-Database.open(__dirname + '/database.sqlite').then(execute)
 
 function execute(db) {
-    db.exec(`
+    return db.exec(`
         CREATE TABLE IF NOT EXISTS proffys (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT,
@@ -12,13 +11,13 @@ function execute(db) {
         );
 
         CREATE TABLE IF NOT EXISTS classes (
-            id INTEGER PRIMARY KEY AUTOINCREMENT
-            subject TEXT,
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            subject INTEGER,
             cost TEXT,
             proffy_id INTEGER
         );
 
-        CREATE TABLE IF NOT EXISTS class_schedule(
+        CREATE TABLE IF NOT EXISTS class_schedule (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             class_id INTEGER,
             weekday INTEGER,
@@ -27,3 +26,5 @@ function execute(db) {
         );
     `)    
 }
+
+module.exports = Database.open(__dirname + '/database.sqlite').then(execute)
